a0dd1119f8018e7d9240907cfff7b3b9e8594d49
ddojai <favianred@gmail.com>
2022-06-15 23:16:56 +0900

    쉽고 빠른 플러터 앱 개발
    - 2.5.1 state

--- flutter_basic_01/lib/screens/first_screen.dart
+++ flutter_basic_01/lib/screens/first_screen.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basic_01/screens/second_screen.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int count = 0;
+
+  void increase() {
+    setState(() {
+      count = count + 1;
+    });
+  }
+
+  void decrease() {
+    setState(() {
+      count = count - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('카운터 앱'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('This is First Screen'),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => SecondScreen(
-                        screenData: 'Data from FirstScreen',
-                      ),
-                    ),
-                  );
-                },
-                child: Text('Go to Second Screen'))
+            Text('카운트: $count', style: TextStyle(fontSize: 25)),
+            Padding(padding: EdgeInsets.all(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: decrease, child: Text('-감소')),
+                ElevatedButton(onPressed: increase, child: Text('+증가')),
+              ],
+            )
           ],
         ),
       ),
