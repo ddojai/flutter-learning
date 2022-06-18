87345d16b0ff48c580ea24b4a5824a6896cbf9cb
ddojai <favianred@gmail.com>
2022-06-18 20:46:03 +0900

    3.4.1 세부 정보 화면 UI 만들기

--- flutter_book_list/lib/screens/detail_screen.dart
+++ flutter_book_list/lib/screens/detail_screen.dart
@@ -5,9 +5,53 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('책 제목'),
+        title: Text('패키지 없이 R로 구현하는 심층 강화학습'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Image.network(
+              'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCuoqW%2Fbtq8uatukHu%2FO0VapTwcTTpV3T29lqMYd0%2Fimg.png'),
+          Padding(
+            padding: EdgeInsets.all(3),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        '패키지 없이 R로 구현하는 심층 강화학습',
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '손으로 풀어보는 Q-Learning부터 R로 구현하는 심층 강화학습까지',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
