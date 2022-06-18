9649a51ed0c5d016ba8e78bae406a3940b50a8de
ddojai <favianred@gmail.com>
2022-06-18 20:02:24 +0900

    3.3.2 BookTile

--- flutter_book_list/lib/screens/list_screen.dart
+++ flutter_book_list/lib/screens/list_screen.dart
@@ -8,33 +8,56 @@ class ListScreen extends StatelessWidget {
         title: Text('도서 목록 앱'),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            title: Text('패키지 없이 R로 구현하는 심층 강화학습'),
-            leading: Image.network(
-              'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCuoqW%2Fbtq8uatukHu%2FO0VapTwcTTpV3T29lqMYd0%2Fimg.png'
-            ),
+        children: <Widget>[
+          BookTile(
+            title: '패키지 없이 R로 구현하는 심층 강화학습',
+            subtitle: '부제 1',
+            description: '설명 1',
+            image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCuoqW%2Fbtq8uatukHu%2FO0VapTwcTTpV3T29lqMYd0%2Fimg.png',
           ),
-          ListTile(
-            title: Text('바로 찾아 바로 만는 포토샵 콘텐츠 디자인 북'),
-            leading: Image.network(
-                'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Flzlyb%2Fbtq8nD5gYAf%2FiuHnWoFZPoBM35Y89aQZb0%2Fimg.png'
-            ),
+          BookTile(
+            title: '바로 찾아 바로 만는 포토샵 콘텐츠 디자인 북',
+            subtitle: '부제 2',
+            description: '설명 2',
+            image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Flzlyb%2Fbtq8nD5gYAf%2FiuHnWoFZPoBM35Y89aQZb0%2Fimg.png',
           ),
-          ListTile(
-            title: Text('Vue.js 프로젝트 투입 일주일 전'),
-            leading: Image.network(
-                'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbGOxQ8%2Fbtq6imQpvmA%2FEY3gKphHOPQbk8KT5FOm8K%2Fimg.jpg'
-            ),
+          BookTile(
+            title: 'Vue.js 프로젝트 투입 일주일 전',
+            subtitle: '부제 3',
+            description: '설명 3',
+            image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbGOxQ8%2Fbtq6imQpvmA%2FEY3gKphHOPQbk8KT5FOm8K%2Fimg.jpg',
           ),
-          ListTile(
-            title: Text('파이썬 해킹 레시피'),
-            leading: Image.network(
-                'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FrTh6k%2Fbtq517fdjqN%2FumbKQy7r9eGVnK4fkC8orK%2Fimg.png'
-            ),
+          BookTile(
+            title: '파이썬 해킹 레시피',
+            subtitle: '부제 4',
+            description: '설명 4',
+            image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FrTh6k%2Fbtq517fdjqN%2FumbKQy7r9eGVnK4fkC8orK%2Fimg.png',
           )
         ],
       ),
+    );
+  }
+}
+
+class BookTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String description;
+  final String image;
+
+  BookTile({
+    required this.title,
+    required this.subtitle,
+    required this.description,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: Image.network(image),
+      onTap: () {},
     );
   }
 }
