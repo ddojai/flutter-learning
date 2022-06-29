7c0431babc00aaa9879dd734e7fd255bc6c31a22
ddojai <favianred@gmail.com>
2022-06-29 23:24:01 +0900

    Todo app
    - 5.2 initState()
    - 5.3 moveScreen()
    - 5.4 shared_preferences
    - 5.5 checkLogin()
    - 5.6 ListScreen
    - 5.7 LoginScreen

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: SplashScreen(),
     );
   }
 }
