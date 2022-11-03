0adad5b212b90e7db29d03defdc70025af5f751a
ddojai <favianred@gmail.com>
2022-11-03 23:47:39 +0900

    5.3 sqlite로 Todo 기능 구현하기

--- flutter_todo/lib/screens/list_screen.dart
+++ flutter_todo/lib/screens/list_screen.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/models/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<Todo> todos = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    print("initState");
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('ListScreen')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('할 일 목록 앱'),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.book),
+                  Text('뉴스'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text('+', style: TextStyle(fontSize: 25)),
+        onPressed: () {},
+      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.separated(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(todos[index].title),
+                  onTap: () {},
+                  trailing: Container(
+                    width: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: InkWell(
+                            child: Icon(Icons.edit),
+                            onTap: () {},
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: InkWell(
+                            child: Icon(Icons.delete),
+                            onTap: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+            ),
     );
   }
 }
